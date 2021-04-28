80c8c6820a5846e0b46aa4c41598e448b4db7b99
CrimsonCodex2195 <shariqali2195@gmail.com>
2021-04-27 22:58:02 -0400

    Completed Fundamentals 02

---
+++ 02-Fundamentals-Part-2/script.js
@@ -0,0 +1,22 @@
+const js = 'amazing';
+console.log(40+8+23-10);
+
+console.log('Shariq');
+console.log(23);
+
+const firstName = 'Shariq'
+const first = 'shariq'
+const firstNamePerson;
+const first_name;
+
+console.log(firstName);
+console.log(firstName);
+// Illegal Variable Names Include &, $, or numbers
+// Variables that are keywords are also not allowed. EX.
+//  true, function, if, NAME (may work sometimes)
+
+// Avoid starting first names of variables capitalized. Try your best to follow camelConvention.
+// Variable names are descriptive and TRUE CONSTANTS are always capitalized.
+
+const firstJob = 'Unemployed';
+const currentJob = 'Unemployed'; // I cry

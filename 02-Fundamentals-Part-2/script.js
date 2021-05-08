b383aea963abf8e39e464105de1de2a5d28b415a
Shariq Ali <shariqali2195@gmail.com>
2021-05-07 20:48:33 -0400

    Compled some lecture assignments and added coding-challenges.pdf

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -15,13 +15,47 @@ console.log(typeof isIsland, typeof population, typeof country, typeof language)
 console.log(`If the country was spit in half, each half of the population would hold ${population / 2} people`);
 console.log(population++);
 
-
 let populationFinland = 6000000;
 if(population > populationFinland){
-  return 'My country has more people than Finland.';
+  return 'My country has more people than Finland.'
 }
 
 let description = 'Portugal is in Europe and its 11 million people speak portuguese.'
 
 
-// LECTURE: Strings and Template Literals
+// LECTURE: Taking Descisions: if / else Statemetns
+if(population > 33000000000){
+  console.log("Portugal's population is above average.")
+}
+else{
+  console.log(`Portugals population is ${33000000000 - population} below average.`)
+}
+
+
+// CODING CHALLENGE #1
+// BMI = mass / height ** 2 = mass / (height * height) (mass in kg and height in meter)
+const markData1 = {
+  'Height': 1.19, //Meter
+  'Weight': 78 // Kilograms
+}
+
+const markData2 = {
+  'Height': 1.88, //Meter
+  'Weight': 95 // Kilograms
+}
+
+const johnData1 = {
+  'Height': 1.95, //Meter
+  'Weight': 92 // Kilograms
+}
+
+const johnData2 = {
+  'Height': 1.76, //Meter
+  'Weight': 85 // Kilograms
+}
+
+function calculateBMI(object){
+  return object.Mass / object.height ** 2;
+}
+
+console.log(calculateBMI(markData1), calculateBMI(markData2), calculateBMI(johnData1), calculateBMI(johnData2));
